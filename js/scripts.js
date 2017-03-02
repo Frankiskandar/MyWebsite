727edf0cd4adb2e95fc1415194c26bd2c747d426
Frank <fnu.frangky@gmail.com>
2017-03-02 18:55:47 -0500

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+  document.getElementById("navBar").style.display = 'none';
+};
+
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        var target = $(this.hash);
+
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 900);
+        }
+    });
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 0) {
+        $("#navBar").fadeIn("slow");
+    } else {
+        $("#navBar").fadeOut("slow");
+    }
+});
